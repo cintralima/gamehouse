70f525bb029ef8a4a79a02012b2a8f394ed0e722
Leo <leocintra26@gmail.com>
2021-02-25 15:59:00 -0300

    Criação dos Repository

---
+++ src/main/java/com/gamehouse/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.gamehouse.repository;
+
+import com.gamehouse.models.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
---
+++ src/main/java/com/gamehouse/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.gamehouse.repository;
+
+import com.gamehouse.models.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
